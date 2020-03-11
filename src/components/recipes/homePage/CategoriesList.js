6e744d6c6ede425a887cd7750cdb1ea099f14402
Vaiva Brazaityte <vabr@danskebank.lt>
2020-03-11 22:40:10 +0200

    dynamic categories

--- src/components/recipes/homePage/CategoriesList.js
+++ src/components/recipes/homePage/CategoriesList.js
@@ -2,6 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+export default function CategoriesList(props) {
+  return (
+    <CategoriesContainer>
+      <Header>Kategorijos:</Header>
+      <ul>
+        {props.categories.map(category => {
+          return (
+            <li>
+              <h5>{category}</h5>
+            </li>
+          );
+        })}
+      </ul>
+    </CategoriesContainer>
+  );
+}
+
+CategoriesList.propTypes = {
+  categories: PropTypes.array
+};
+
 const CategoriesContainer = styled.div`
   flex: 1;
   padding: 95px 0px 0px 30px;
@@ -10,29 +31,3 @@ const CategoriesContainer = styled.div`
 const Header = styled.h3`
   padding-bottom: "6px";
 `;
-
-export default function CategoriesList(props) {
-  return (
-    <CategoriesContainer>
-      <Header>Kategorijos:</Header>
-      <ul>
-        <li>
-          <h5>pusryčiai</h5>
-        </li>
-        <li>
-          <h5>pietūs</h5>
-        </li>
-        <li>
-          <h5>vakarienė</h5>
-        </li>
-        <li>
-          <h5>gėrimai</h5>
-        </li>
-      </ul>
-    </CategoriesContainer>
-  );
-}
-
-CategoriesList.propTypes = {
-  recipes: PropTypes.object
-};
